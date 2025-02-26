c26e2fa3c4605ee30ec24fdf1245a5e8203538cc
Yang-9987 <1013404703@qq.com>
2025-02-26 16:14:07 +0800

    feat(utils): 为 MockMethod 添加正则表达式支持
    - 在 generateMockTemplate 函数中增加了对正则表达式类型的支持
    - 解析并生成正则表达式

--- src/utils/MockMethod.ts
+++ src/utils/MockMethod.ts
@@ -4,6 +4,9 @@ export const generateMockTemplate = (fields) => {
             template[field.name] = generateMockTemplate(field.children)
         } else if (field.type === 'array' && field.children) {
             template[field.name + '|1-10'] = [generateMockTemplate(field.children)]
+        } else if (field.type === 'regexp') {
+            let regexpString = field.rule.replaceAll("/", "");
+            template[field.name] = new RegExp(regexpString)
         } else {
             template[field.name] = field.rule
         }
